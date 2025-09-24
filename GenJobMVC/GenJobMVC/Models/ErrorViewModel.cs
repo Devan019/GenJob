e2f8bd8aee43931c0cf0b4d180efc7f4936682ce
Nishant Dholakia <nishantdholakia2020@gmail.com>
2025-09-24 20:39:21 +0530

    Added the Base of .NET core MVC version-8.0(LTS). added proper gitignore files too

---
+++ GenJobMVC/GenJobMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GenJobMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
